767cf41485d6113519aeb5e0a431d1b5d1c9f0fb
johannes <johannes@artificialmuseum.com>
2024-02-27 12:01:53 +0100

    undefined

--- src/routes/hooks.server.ts
+++ src/routes/hooks.server.ts
@@ -17,7 +17,7 @@ export const handle: Handle = async ({ resolve, event }) => {
   }
 
   const response = await resolve(event);
-  if (event.url.pathname.startsWith('/api')) {
+  if (event.url.pathname.startsWith('/')) {
         response.headers.append('Access-Control-Allow-Origin', `*`);
   }
   return response;
