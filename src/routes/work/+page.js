4a16f45225b3b1af7d80dfbb494c545de58ff651
johannes <johannes@artificialmuseum.com>
2024-03-01 11:48:17 +0100

    undefined

--- src/routes/work/+page.js
+++ src/routes/work/+page.js
@@ -2,6 +2,14 @@ import {pb} from "$lib/db"
 
 
 export const load = async () => {
+	// const response = new Response('Hello SvelteKit', {
+    //     headers: {
+    //         'Access-Control-Allow-Origin': '*', // Specify the url you wish to permit
+    //         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    //         'Access-Control-Allow-Headers': 'Content-Type'
+    //     }
+    // });
+
 	const kategorien = await pb.collection('Kategorien').getFullList({
 		sort: 'Kategorie_Name'
 	})
@@ -12,7 +20,13 @@ export const load = async () => {
 	})
 
 	return {
+		response,
         kategorien,
 		projekte
 	}
 }
+
+
+// export async function load ({ fetch, params}) {
+// 	const res = await.fetch('')
+// }
