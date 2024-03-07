7565f68b7f651ddbd26a95decf802bfca29a9ff3
johannes <johannes@artificialmuseum.com>
2024-03-07 12:30:31 +0100

    undefined

--- src/lib/utils/getUrls.js
+++ src/lib/utils/getUrls.js
@@ -1,7 +1,7 @@
 // @ts-nocheck
 // export const url = "http://127.0.0.1:8090" // dev local
 // export const url = "https://krumboeck-backend.cr.fugbach.one" // server
-export const url = "https://cms.krumboeck.xyz/"  // pretty
+export const url = "https://cms.krumboeck.xyz"  // pretty
 
 export const getImageURL = (collectionId, recordId, fileName, size = '0x0') => {
 	return `/api/files/${collectionId}/${recordId}/${fileName}?thumb=${size}`
